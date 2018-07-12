bb5c741c454ed8eed382466945bfaccfa112eb1d
Brandon Carlson <bcarlso@gmail.com>
2018-07-12 16:20:54 -0500

    Additional setup to get things running

--- dotnet-core.sh
+++ dotnet-core.sh
@@ -1,3 +1,7 @@
+if command -v rvm 2>/dev/null; then
+  ./ruby.sh
+fi
+rvm install ruby-2.3.3
 brew cask install dotnet
 brew cask install dotnet-sdk
 brew cask install visual-studio-code
---
+++ ruby.sh
@@ -0,0 +1,3 @@
+brew install gpg
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
