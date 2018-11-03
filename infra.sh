385f764db56560a6ac54c3cc6313e921da052d71
Brandon Carlson <bcarlso@leantechniques.com>
2018-11-03 10:15:55 -0700

    Fighting to get AWS CLI tools installed

--- infra.sh
+++ infra.sh
@@ -1,2 +1,9 @@
 brew cask install docker
 brew install postgres
+
+# Jump through hoops to install AWS command line utilities
+mkdir /usr/local/Frameworks
+sudo chown $(whoami):admin /usr/local/Frameworks
+brew remove python
+brew install python3
+brew install awscli
